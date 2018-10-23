7c443f7d6cd4a3eb5f23c1ca341e7f51918cb03e
SS <SS@DESKTOP-LT1LE1L>
2018-10-23 21:09:08 +0900

    #2 task2

--- src/test/java/edu/handong/csee/pp1/dp/MainTest.java
+++ src/test/java/edu/handong/csee/pp1/dp/MainTest.java
@@ -26,5 +26,18 @@ public class MainTest {
 		// TASK2 TODO GitHub에서 작은 규모의 자바 프로젝트를 2개를 찾아, arff파일을 생성하시오. 파일이름은 [project이름].arff data디렉터리에 저장되게 하고
 		// 생성이 된 후 commit/push해야 함.
 		// 위 test main을 참고하여 실행하면 됨.
+		String[] args = {"C:\\git\\JavaStuff","data/JavaStuff.arff"};
+		// String[] args = {"/use/this/style/for/your/path"}; // for mac users
+		
+		assertEquals(args.length,2);
+		
+		Main.main(args);
+		
+		String[] args2 = {"C:\\git\\java","data/java.arff"};
+		// String[] args = {"/use/this/style/for/your/path"}; // for mac users
+		
+		assertEquals(args2.length,2);
+		
+		Main.main(args2);
 	}
 }
